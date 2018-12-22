815b9630bd174d8c7d07cd902e86ccb4ae83bacd
etudiant <theo.henry@orange.fr>
2018-12-22 16:28:03 -0500

    gestion des actions

--- Pito/src/main/scala/FightData.scala
+++ Pito/src/main/scala/FightData.scala
@@ -1,25 +1,28 @@
+import breeze.optimize.MaxIterations
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
-import math.{ sqrt, pow }
+
+import math.{pow, sqrt}
 
 
 object FightData {
   class Dice(
               var number: Int,
               var range: Int,
-              var bonus: Int
+              var bonus: Int,
+              var crit: Int
             )
 
   class Creature(
                 var name: String,
                 var armure: Int,
                 var hp: Int,
-                var melee_dmg: Array[Int],
+                var melee_touch: Array[Int],
                 var throws: Array[Dice],
                 var melee_range: Int,
-                var ranged_dmg: Array[Int],
+                var ranged_touch: Array[Int],
                 var ranged_range: Int,
                 var regen: Int,
                 var distance: Int,
@@ -31,21 +34,178 @@ object FightData {
       s"$name"
   }
 
+
+
+  def sendAction(ctx: EdgeContext[FightData.Creature,String,Long]):Unit = {
+    var dmg = 0
+    var heal = 0
+    val r = scala.util.Random
+    if (ctx.dstAttr.name == "Solar" && ctx.srcAttr.dead == false && ctx.dstAttr.dead == false) {
+      var distance = ctx.srcAttr.distance
+
+      if (distance <= ctx.srcAttr.melee_range) {
+        for (i <- 1 to ctx.srcAttr.melee_touch.size) {
+          val attack = ctx.srcAttr.melee_touch.apply(i)
+          val roll = r.nextInt(20)
+          val dstHP = ctx.dstAttr.hp
+
+          if (roll == 20) {
+            val diceNumber = ctx.srcAttr.throws.apply(1).number
+            val diceRange = ctx.srcAttr.throws.apply(1).range
+            val dmgBonus = ctx.srcAttr.throws.apply(1).bonus
+            for (i <- 1 to diceNumber) {
+              dmg = dmg + r.nextInt(diceRange)
+            }
+            dmg = dmg + dmgBonus
+            dmg = dmg * ctx.srcAttr.throws.apply(1).crit
+
+          } else {
+            if (roll + attack >= ctx.dstAttr.armure) {
+              val diceNumber = ctx.srcAttr.throws.apply(1).number
+              val diceRange = ctx.srcAttr.throws.apply(1).range
+              val dmgBonus = ctx.srcAttr.throws.apply(1).bonus
+              for (i <- 1 to diceNumber) {
+                dmg = dmg + r.nextInt(diceRange)
+              }
+              dmg = dmg + dmgBonus
+
+            }
+          }
+        }
+
+      } else {
+        if (distance <= ctx.srcAttr.ranged_range) {
+          val attack = ctx.srcAttr.ranged_touch.apply(0)
+          val roll = r.nextInt(20)
+          val dstHP = ctx.dstAttr.hp
+
+          if (roll == 20) {
+            val diceNumber = ctx.srcAttr.throws.apply(2).number
+            val diceRange = ctx.srcAttr.throws.apply(2).range
+            val dmgBonus = ctx.srcAttr.throws.apply(2).bonus
+            for (i <- 1 to diceNumber) {
+              dmg = dmg + r.nextInt(diceRange)
+            }
+            dmg = dmg + dmgBonus
+            dmg = dmg * ctx.srcAttr.throws.apply(2).crit
+
+          } else {
+            if (roll + attack >= ctx.dstAttr.armure) {
+              val diceNumber = ctx.srcAttr.throws.apply(2).number
+              val diceRange = ctx.srcAttr.throws.apply(2).range
+              val dmgBonus = ctx.srcAttr.throws.apply(2).bonus
+              for (i <- 1 to diceNumber) {
+                dmg = dmg + r.nextInt(diceRange)
+              }
+              dmg = dmg + dmgBonus
+
+            }
+          }
+          ctx.srcAttr.distance = distance - ctx.srcAttr.mvmt
+        } else {
+          ctx.srcAttr.distance = distance - ctx.srcAttr.mvmt * 2
+        }
+      }
+
+
+    }
+    if (ctx.srcAttr.name == "Solar" && ctx.srcAttr.dead == false && ctx.dstAttr.dead == false) {
+      val distance = ctx.dstAttr.distance
+      // 2/3 des PVs max du solar
+      if (ctx.srcAttr.hp <= 242) {
+        // Cure Critical Wounds
+        for (i <- 1 to 4) {
+          heal = r.nextInt(8) + heal
+        }
+        heal = heal + 20
+
+      } else {
+
+        if (distance <= ctx.srcAttr.melee_range) {
+          for (i <- 1 to ctx.srcAttr.melee_touch.size) {
+            val attack = ctx.srcAttr.melee_touch.apply(i)
+            val roll = r.nextInt(20)
+            val dstHP = ctx.dstAttr.hp
+
+            if (roll == 20) {
+              val diceNumber = ctx.srcAttr.throws.apply(1).number
+              val diceRange = ctx.srcAttr.throws.apply(1).range
+              val dmgBonus = ctx.srcAttr.throws.apply(1).bonus
+              for (i <- 1 to diceNumber) {
+                dmg = dmg + r.nextInt(diceRange)
+              }
+              dmg = dmg + dmgBonus
+              dmg = dmg * ctx.srcAttr.throws.apply(1).crit
+
+            } else {
+              if (roll + attack >= ctx.dstAttr.armure) {
+                val diceNumber = ctx.srcAttr.throws.apply(1).number
+                val diceRange = ctx.srcAttr.throws.apply(1).range
+                val dmgBonus = ctx.srcAttr.throws.apply(1).bonus
+                for (i <- 1 to diceNumber) {
+                  dmg = dmg + r.nextInt(diceRange)
+                }
+                dmg = dmg + dmgBonus
+
+              }
+            }
+          }
+
+        } else {
+          if (distance <= ctx.srcAttr.ranged_range) {
+            for (i <- 1 to ctx.srcAttr.ranged_touch.size) {
+              val attack = ctx.srcAttr.ranged_touch.apply(i)
+              val roll = r.nextInt(20)
+              val dstHP = ctx.dstAttr.hp
+
+              if (roll == 20) {
+                val diceNumber = ctx.srcAttr.throws.apply(2).number
+                val diceRange = ctx.srcAttr.throws.apply(2).range
+                val dmgBonus = ctx.srcAttr.throws.apply(2).bonus
+                for (i <- 1 to diceNumber) {
+                  dmg = dmg + r.nextInt(diceRange)
+                }
+                dmg = dmg + dmgBonus
+                dmg = dmg * ctx.srcAttr.throws.apply(2).crit
+
+              } else {
+                if (roll + attack >= ctx.dstAttr.armure) {
+                  val diceNumber = ctx.srcAttr.throws.apply(2).number
+                  val diceRange = ctx.srcAttr.throws.apply(2).range
+                  val dmgBonus = ctx.srcAttr.throws.apply(2).bonus
+                  for (i <- 1 to diceNumber) {
+                    dmg = dmg + r.nextInt(diceRange)
+                  }
+                  dmg = dmg + dmgBonus
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    heal = heal + ctx.srcAttr.regen
+    ctx.srcAttr.hp = ctx.srcAttr.hp + heal
+  }
+
+
+
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Fight").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    val dice_solar = new Dice(3, 6, 18)
+    val dice_solar = new Dice(3, 6, 18, 2)
+    val dice_solar2 = new Dice(2, 8, 13, 2)
     val solar = new Creature(
       "Solar",
       44,
       363,
       Array(35, 30, 25, 20),
-      Array(dice_solar),
+      Array(dice_solar,dice_solar2),
       10,
       Array(31, 26, 21, 16),
       110,
-      15,
+      14,
       0,
       50,
       false
@@ -66,13 +226,14 @@ object FightData {
     // Worg riders
     for (i <- 1 to 9) {
       val coord_rider = Array(random.nextInt(200), random.nextInt(200))
-      val dice_rider = new Dice(1, 8, 2)
+      val dice_rider = new Dice(1, 8, 2,3)
+      val dice_rider2 = new Dice(1, 6, 0,3)
       val rider = new Creature(
         "Worg Rider " + i,
         18,
         13,
         Array(6),
-        Array(dice_rider),
+        Array(dice_rider,dice_rider2),
         5,
         Array(4),
         60,
@@ -87,8 +248,8 @@ object FightData {
 
     // Warlord
     val coord_warlord = Array(random.nextInt(200), random.nextInt(200))
-    val dice_warlord1 = new Dice(1, 8, 10)
-    val dice_warlord2 = new Dice(2, 6, 0)
+    val dice_warlord1 = new Dice(1, 8, 10,2)
+    val dice_warlord2 = new Dice(1, 6, 5,2)
     val warlord = new Creature(
       "Warlord",
       27,
@@ -109,13 +270,14 @@ object FightData {
     // Barbares orcs
     for (i <- 1 to 4) {
       val coord_orc = Array(random.nextInt(200), random.nextInt(200))
-      val dice_orc = new Dice(1, 8, 10)
+      val dice_orc = new Dice(1, 8, 10,3)
+      val dice_orc2 = new Dice(1, 8, 6,3)
       val orc = new Creature(
         "Barbare Orc " + i,
         17,
         142,
         Array(19, 14, 9),
-        Array(dice_orc),
+        Array(dice_orc,dice_orc2),
         5,
         Array(16, 11, 6),
         110,
@@ -142,5 +304,9 @@ object FightData {
     val edges: RDD[Edge[String]] =
       sc.parallelize(relations)
     val graph = Graph(vertices, edges, null)
+
+
+
+
   }
 }
